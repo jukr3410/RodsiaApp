7817746ba1cb124b17b8d10d04548afbe73c66ca
pimpcnn <56501828+jukr3410@users.noreply.github.com>
2021-08-13 12:37:46 +0700

    add constants

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// color that use in app
+const textColorBlack = Color(0x38454C);
+const textColorWhite = Colors.white;
+
+const pramaryColor = Color(0xFECE2F);
+const bgColor = Color(0xF2F4F6);
+
+// margin, padding
+const defualtPadding = 20.0;
+
+// message
+const messageError = "Service unvalidable, please try agian.";
