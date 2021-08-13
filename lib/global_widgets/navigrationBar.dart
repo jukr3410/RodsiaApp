075a6c1b80ac08197f34af10ac970bea6f8e958f
Thanuchcha1603 <44134629+Thanuchcha1603@users.noreply.github.com>
2021-08-13 22:23:14 +0700

    add hextocolor

--- lib/global_widgets/navigrationBar.dart
+++ lib/global_widgets/navigrationBar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rodsiaapp/constants.dart';
+import 'package:rodsiaapp/global_widgets/hexTocolor.dart';
 
 class NavigrationBar extends StatefulWidget {
   @override
@@ -8,7 +10,7 @@ class NavigrationBar extends StatefulWidget {
 
 class _NavigrationBarState extends State<NavigrationBar> {
   int _selectedIndex = 0;
-  
+
   void updateTabSelection(int index) {
     setState(() {
       _selectedIndex = index;
@@ -23,105 +25,80 @@ class _NavigrationBarState extends State<NavigrationBar> {
       floatingActionButtonLocation: FloatingActionButtonLocation
           .centerDocked, //specify the location of the FAB
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          updateTabSelection(4);
+        },
         child: Container(
           margin: EdgeInsets.all(15.0),
-          child: ImageIcon(AssetImage("image/findGarage.png")),
+          child: ImageIcon(AssetImage("image/icon-findGarage-navbar.png"),
+              color: _selectedIndex == 4 ? primaryColor : Colors.black),
         ),
         elevation: 4.0,
         backgroundColor: Colors.grey.shade200,
       ),
       bottomNavigationBar: BottomAppBar(
-        child: Container(
-          margin: EdgeInsets.only(left: 12.0, right: 12.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                //update the bottom app bar view each time an item is clicked
-                onPressed: () {
-                  updateTabSelection(0);
-                },
-                iconSize: 27.0,
-                icon: ImageIcon(
-                  AssetImage("image/icon-findGarage-navbar.png"),
-                  //darken the icon if it is selected or else give it a different color
-                  color: _selectedIndex == 0 ? Colors.black : Colors.white,
+          child: Container(
+            margin:
+                EdgeInsets.only(left: defualtPadding, right: defualtPadding),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  //update the bottom app bar view each time an item is clicked
+                  onPressed: () {
+                    updateTabSelection(0);
+                  },
+                  iconSize: 27.0,
+                  icon: ImageIcon(
+                    AssetImage("image/icon-home-navbar.png"),
+                    //darken the icon if it is selected or else give it a different color
+                    color: _selectedIndex == 0 ? Colors.black : Colors.white,
+                  ),
                 ),
-              ),
-              IconButton(
-                onPressed: () {
-                  updateTabSelection(1);
-                },
-                iconSize: 27.0,
-                icon: ImageIcon(
-                  AssetImage("image/icon-findProblem-navbar.png"),
-                  color: _selectedIndex == 1 ? Colors.black : Colors.white,
+                IconButton(
+                  onPressed: () {
+                    updateTabSelection(1);
+                  },
+                  iconSize: 27.0,
+                  icon: ImageIcon(
+                    AssetImage("image/icon-findProblem-navbar.png"),
+                    color: _selectedIndex == 1 ? Colors.black : Colors.white,
+                  ),
                 ),
-              ),
-              //to leave space in between the bottom app bar items and below the FAB
-              SizedBox(
-                width: 50.0,
-              ),
-              IconButton(
-                onPressed: () {
-                  // Navigator.push(
-                  //     context, MaterialPageRoute(builder: (context) => Otp()));
-                  updateTabSelection(2);
-                },
-                iconSize: 27.0,
-                icon: ImageIcon(
-                  AssetImage("image/icon-notify-navbar.png"),
-                  color: _selectedIndex == 2 ? Colors.black : Colors.white,
+                //to leave space in between the bottom app bar items and below the FAB
+                SizedBox(
+                  width: 50.0,
                 ),
-              ),
-              IconButton(
-                onPressed: () {
-                  updateTabSelection(3);
-                },
-                iconSize: 27.0,
-                icon: ImageIcon(
-                  AssetImage("image/icon-profile-navbar.png"),
-                  color: _selectedIndex == 3 ? Colors.black : Colors.white,
+                IconButton(
+                  onPressed: () {
+                    updateTabSelection(2);
+                  },
+                  iconSize: 27.0,
+                  icon: ImageIcon(
+                    AssetImage("image/icon-notify-navbar.png"),
+                    color: _selectedIndex == 2 ? Colors.black : Colors.white,
+                  ),
                 ),
-              ),
-            ],
+                IconButton(
+                  onPressed: () {
+                    updateTabSelection(3);
+                  },
+                  iconSize: 27.0,
+                  icon: ImageIcon(
+                    AssetImage("image/icon-profile-navbar.png"),
+                    color: _selectedIndex == 3 ? Colors.black : Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        //to add a space between the FAB and BottomAppBar
-        shape: CircularNotchedRectangle(),
-        //color of the BottomAppBar
-        color: Color(0xFECE2F),
-      ),
-      // items: const <BottomNavigationBarItem>[
-      //   BottomNavigationBarItem(
-      //     icon: ImageIcon(AssetImage("image/icon-home-navbar.png")),
-      //     label: 'Home',
-      //   ),
-      //   BottomNavigationBarItem(
-      //       icon: ImageIcon(AssetImage("image/icon-findProblem-navbar.png")),
-      //       label: 'Find-problem'),
-      //   BottomNavigationBarItem(
-      //       icon: ImageIcon(AssetImage("image/logo.png")),
-      //       label: 'Find-Garage'),
-      //   BottomNavigationBarItem(
-      //       icon: ImageIcon(AssetImage("image/icon-notify-navbar.png")),
-      //       label: 'Notify'),
-      //   BottomNavigationBarItem(
-      //       icon: ImageIcon(AssetImage("image/icon-profile-navbar.png")),
-      //       label: 'Profile'),
-      // ],
-      // currentIndex: _selectedIndex,
-      // fixedColor: Colors.black,
-      // type: BottomNavigationBarType.fixed,
-      // selectedIconTheme: IconThemeData(size: 30),
-      // showUnselectedLabels: false,
-      // showSelectedLabels: false,
-      // unselectedIconTheme: IconThemeData(size: 24),
-      // onTap: _onItemTapped,
-      // backgroundColor: hexToColor("#FECE2F"),
-      // ),
+          //to add a space between the FAB and BottomAppBar
+          shape: CircularNotchedRectangle(),
+          //color of the BottomAppBar
+          color: hexToColor("#FECE2F"),
+          ),
+      
     );
   }
 }
