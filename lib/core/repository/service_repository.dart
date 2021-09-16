66be3c5cb8bd4dbebc61926e7903b1a339255d31
juk <56501828+jukr3410@users.noreply.github.com>
2021-09-16 21:48:18 +0700

    [update] api repo

---
+++ lib/core/repository/service_repository.dart
@@ -0,0 +1,18 @@
+import 'package:rodsiaapp/core/models/service_model.dart';
+
+import 'service_api.dart';
+
+class ServiceRepository {
+  ServiceApi serviceApi = ServiceApi();
+  static final ServiceRepository _serviceRepository = ServiceRepository._();
+
+  ServiceRepository._();
+
+  factory ServiceRepository() {
+    return _serviceRepository;
+  }
+
+  Future<List<Service>> getServiceByGarage({required String garageId}) async {
+    return await serviceApi.getServiceByGarage(garageId: garageId);
+  }
+}
